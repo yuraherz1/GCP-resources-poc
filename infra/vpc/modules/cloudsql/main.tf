68080f417de23fa633f7d4627470dbdaf9d7b465
Yurii Herz <yuraherz@gmail.com>
2026-01-24 13:53:59 +0200

    mysql 05

--- infra/vpc/modules/cloudsql/main.tf
+++ infra/vpc/modules/cloudsql/main.tf
@@ -4,18 +4,31 @@ resource "google_sql_database_instance" "main" {
   name             = "mysql-test-default"
   database_version = "MYSQL_8_0"
   settings {
-    tier = "db-f1-micro"
+    # tier = "db-f1-micro"
+    tier = "db-custom-2-4096"
     ip_configuration {
       psc_config {
-        psc_enabled               = true
-        allowed_consumer_projects = ["infra-demo-qa"] #allowed-consumer-project-name
+        psc_enabled               = null
+        allowed_consumer_projects = []
+        network_attachment_uri    = null
+
+        psc_auto_connections {
+          consumer_network            = "projects/infra-demo-qa/global/networks/default"
+          consumer_network_status     = "VALID"
+          consumer_service_project_id = "infra-demo-qa"
+          ip_address                  = "10.186.0.5"
+          status                      = "ACTIVE"
+        }
+
+        psc_auto_connections {
+          consumer_network            = "projects/infra-demo-dev/global/networks/default"
+          consumer_network_status     = "VALID"
+          consumer_service_project_id = "infra-demo-dev"
+          ip_address                  = "10.186.0.6"
+          status                      = "ACTIVE"
+        }
+
       }
-      ipv4_enabled = false
     }
-    # backup_configuration {
-    #   enabled            = true
-    #   binary_log_enabled = true
-    # }
-    availability_type = "REGIONAL"
   }
 }
