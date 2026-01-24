a163f570df40379ccc90c2f41da9cc12bc61e629
Yurii Herz <yuraherz@gmail.com>
2026-01-24 14:07:59 +0200

    asasas

--- infra/vpc/modules/cloudsql/main.tf
+++ infra/vpc/modules/cloudsql/main.tf
@@ -3,15 +3,13 @@ resource "google_sql_database_instance" "main" {
   project          = "infra-demo-dev"
   name             = "mysql-test-default"
   database_version = "MYSQL_8_0"
+
   settings {
-    # tier = "db-f1-micro"
-    tier              = "db-custom-2-4096"
+    tier              = "db-custom-2-4096" #"db-f1-micro"
     availability_type = "REGIONAL"
 
     ip_configuration {
-      ipv4_enabled = false
       psc_config {
-        # psc_enabled               = null
         psc_enabled               = true
         allowed_consumer_projects = []
         network_attachment_uri    = null
@@ -25,7 +23,6 @@ resource "google_sql_database_instance" "main" {
           consumer_network            = "projects/infra-demo-dev/global/networks/default"
           consumer_service_project_id = "infra-demo-dev"
         }
-
       }
     }
   }
