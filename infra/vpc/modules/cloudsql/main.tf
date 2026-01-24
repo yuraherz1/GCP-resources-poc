718c0a733b3c9e7604424bae942026dacc6c36d3
Yurii Herz <yuraherz@gmail.com>
2026-01-24 13:56:35 +0200

    mysql 06

--- infra/vpc/modules/cloudsql/main.tf
+++ infra/vpc/modules/cloudsql/main.tf
@@ -14,18 +14,12 @@ resource "google_sql_database_instance" "main" {
 
         psc_auto_connections {
           consumer_network            = "projects/infra-demo-qa/global/networks/default"
-          consumer_network_status     = "VALID"
           consumer_service_project_id = "infra-demo-qa"
-          ip_address                  = "10.186.0.5"
-          status                      = "ACTIVE"
         }
 
         psc_auto_connections {
           consumer_network            = "projects/infra-demo-dev/global/networks/default"
-          consumer_network_status     = "VALID"
           consumer_service_project_id = "infra-demo-dev"
-          ip_address                  = "10.186.0.6"
-          status                      = "ACTIVE"
         }
 
       }
