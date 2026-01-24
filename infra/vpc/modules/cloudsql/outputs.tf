238aabd803256780e9390da3895f7f9a1e82b85d
Yurii Herz <yuraherz@gmail.com>
2026-01-24 14:38:23 +0200

    assasaasas

--- infra/vpc/modules/cloudsql/outputs.tf
+++ infra/vpc/modules/cloudsql/outputs.tf
@@ -3,8 +3,8 @@ output "mysql_dns_name" {
   value       = google_sql_database_instance.main.dns_name
 }
 
-output "ip_address_psc_" {
-  description = "The DNS name of the DB instance."
-  value       = google_sql_database_instance.main.dns_name
-}
-settings.ip_configuration.psc_config.psc_auto_connections.ip_address
+# output "ip_address_psc_" {
+#   description = "The DNS name of the DB instance."
+#   value       = google_sql_database_instance.main.dns_name
+# }
+# settings.ip_configuration.psc_config.psc_auto_connections.ip_address
