b243dea02221d2d1c5df833d640d317b10c68837
Yurii Herz <yuraherz@gmail.com>
2026-01-24 14:52:16 +0200

    asasas

--- infra/vpc/modules/cloudsql/outputs.tf
+++ infra/vpc/modules/cloudsql/outputs.tf
@@ -5,6 +5,8 @@ output "mysql_dns_name" {
 
 output "ip_address_psc_" {
   description = "The IP address of the consumer endpoint."
-  value       = google_sql_database_instance.main.settings.ip_configuration.psc_config.psc_auto_connections.ip_address
+  value       = values(google_sql_database_instance.main)[*].settings.ip_configuration.psc_config.psc_auto_connections.ip_address
+
+  # value       = google_sql_database_instance.main.settings.ip_configuration.psc_config.psc_auto_connections.ip_address
 
 }
