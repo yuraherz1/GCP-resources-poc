193e51c7b877822a497867722aaadc91645114ad
Yurii Herz <yuraherz@gmail.com>
2026-01-24 15:25:11 +0200

    asasas

--- infra/vpc/main.tf
+++ infra/vpc/main.tf
@@ -50,7 +50,7 @@ resource "google_dns_record_set" "mysql_private_zone" {
   name         = module.mysql_db.mysql_dns_name
   type         = "A"
   ttl          = 300
-  rrdatas      = [module.mysql_db.ip_address_psc_endpoint] #10.186.0.6
+  rrdatas      = ["10.186.0.6"] #10.186.0.6
 }
 
 
