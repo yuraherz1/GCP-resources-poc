e9cfdbf1d0a5ec7085dee2d37d5c419df6856581
Yurii Herz <yuraherz@gmail.com>
2026-01-26 21:53:26 +0200

    asaadasdasdsa

--- infra/vpc/main.tf
+++ infra/vpc/main.tf
@@ -91,11 +91,11 @@ resource "google_compute_instance" "main" {
 
   metadata_startup_script = "echo hi > /test.txt"
 
-  service_account {
-    # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
-    email  = google_service_account.default.email
-    scopes = ["cloud-platform"]
-  }
+  # service_account {
+  #   # Google recommends custom service accounts that have cloud-platform scope and permissions granted via IAM Roles.
+  #   email  = google_service_account.default.email
+  #   scopes = ["cloud-platform"]
+  # }
 }
 
 import {
