695ad8edcd1f387a654abf3d16b9b83d7383bd4e
Yurii Herz <yuraherz@gmail.com>
2026-01-27 17:20:07 +0200

    asasaaasd

--- infra/vpc/modules/cloudsql-postgres/main.tf
+++ infra/vpc/modules/cloudsql-postgres/main.tf
@@ -1,33 +1,35 @@
-# resource "random_password" "db_password" {
-#   length           = 16
-#   special          = true
-#   override_special = "!@#$%^&*"
-#   min_upper        = 1
-#   min_lower        = 1
-#   min_numeric      = 1
-#   keepers          = {}
-# }
+resource "random_password" "db_password" {
+  length           = 16
+  special          = true
+  override_special = "!@#$%^&*"
+  min_upper        = 1
+  min_lower        = 1
+  min_numeric      = 1
+  keepers          = {}
+}
 
-# resource "google_secret_manager_secret" "db_secret" {
-#   secret_id = "POSTGRES_DATA_PASS"
-#   replication {
-#     auto {}
-#   }
+resource "google_secret_manager_secret" "db_secret" {
+  project   = var.data_project_id
+  secret_id = "POSTGRES_DATA_PASS"
+  replication {
+    auto {}
+  }
 
-#   # # Optional: add labels
-#   # labels = {
-#   #   environment = "dev"
-#   # }
-# }
+  # # Optional: add labels
+  # labels = {
+  #   environment = "dev"
+  # }
+}
 
-# resource "google_secret_manager_secret_version" "db_secret_version" {
-#   secret      = google_secret_manager_secret.db_secret.id
-#   secret_data = random_password.db_password.result
+resource "google_secret_manager_secret_version" "db_secret_version" {
+  project     = var.data_project_id
+  secret      = google_secret_manager_secret.db_secret.id
+  secret_data = random_password.db_password.result
 
-#   lifecycle {
-#     prevent_destroy = true
-#   }
-# }
+  lifecycle {
+    prevent_destroy = true
+  }
+}
 
 resource "google_sql_database_instance" "main" {
   project          = var.data_project_id
@@ -51,11 +53,12 @@ resource "google_sql_database_instance" "main" {
   }
 }
 
-# resource "google_sql_user" "users" {
-#   name     = "mte"
-#   instance = google_sql_database_instance.main.name
-#   password = random_password.db_password.result
-# }
+resource "google_sql_user" "users" {
+  project  = var.data_project_id
+  name     = "mte"
+  instance = google_sql_database_instance.main.name
+  password = random_password.db_password.result
+}
 
 # resource "google_sql_database_instance" "main" {
 #   project          = var.data_project_id
