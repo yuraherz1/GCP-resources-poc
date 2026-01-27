229482132b0ae67f7863f7c805dc34dccd5d58c8
Yurii Herz <yuraherz@gmail.com>
2026-01-27 13:10:33 +0200

    asdasdsaasdasd

--- infra/vpc/main.tf
+++ infra/vpc/main.tf
@@ -82,7 +82,7 @@ resource "google_compute_instance" "main" {
 
   boot_disk {
     initialize_params {
-      image = "ubuntu-os-cloud/ubuntu-2404-lts"
+      image = "ubuntu-os-cloud/ubuntu-2404-lts-amd64"
       size  = 10
       type  = "pd-balanced"
     }
