4a2963472609a8448b628daef942b881685670e8
Yurii Herz <yuraherz@gmail.com>
2026-01-28 17:48:55 +0200

    asaadadasdsa

--- infra/vpc/api-services.tf
+++ infra/vpc/api-services.tf
@@ -1,31 +1,24 @@
 locals {
-  projects = {
-    "${var.project_id}" = {
-      services = [
-        "us-east-1",
-        "sqladmin.googleapis.com",
-        "networkconnectivity.googleapis.com",
-        "compute.googleapis.com",
-        "vpcaccess.googleapis.com",
-        "dns.googleapis.com",
-      ]
-    }
-    "${var.data_project_id}" = {
-      services = [
-        "sqladmin.googleapis.com",
-        "networkconnectivity.googleapis.com",
-        "compute.googleapis.com",
-        "vpcaccess.googleapis.com",
-        "dns.googleapis.com",
-      ]
-    }
-  }
+  services = [
+    "sqladmin.googleapis.com",
+    "networkconnectivity.googleapis.com",
+    "compute.googleapis.com",
+    "vpcaccess.googleapis.com",
+    "dns.googleapis.com",
+  ]
 }
 
-resource "google_project_service" "api" {
-  for_each           = local.projects
-  project            = each.key
-  service            = each.value.services
+resource "google_project_service" "api_app" {
+  for_each           = toset(local.services)
+  project            = var.project_id
+  service            = each.key
+  disable_on_destroy = false
+}
+
+resource "google_project_service" "api_data" {
+  for_each           = toset(local.services)
+  project            = var.data_project_id
+  service            = each.key
   disable_on_destroy = false
 }
 
@@ -33,6 +26,7 @@ resource "time_sleep" "wait_for_apis" {
   create_duration = "60s"
 
   depends_on = [
-    google_project_service.api,
+    google_project_service.api_app,
+    google_project_service.api_data
   ]
 }
