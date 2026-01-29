8985298c488e313f4e74551f2645db03851598ba
Yurii Herz <yuraherz@gmail.com>
2026-01-29 13:41:48 +0200

    AADASADASDAS

--- infra/vpc/data.tf
+++ infra/vpc/data.tf
@@ -1,9 +1,9 @@
-data "google_compute_network" "existing_network_data" {
-  name    = "default"
-  project = var.data_project_id #"infra-demo-dev"
-}
+# data "google_compute_network" "existing_network_data" {
+#   name    = "default"
+#   project = var.data_project_id #"infra-demo-dev"
+# }
 
-data "google_compute_network" "existing_network_app" {
-  name    = "default"
-  project = var.project_id #"infra-demo-qa"
-}
+# data "google_compute_network" "existing_network_app" {
+#   name    = "default"
+#   project = var.project_id #"infra-demo-qa"
+# }
