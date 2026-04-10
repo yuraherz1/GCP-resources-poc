5a48103a3798c278b93fee38944cd9772dc45e68
yuraherz1 <yuraherz@gmail.com>
2026-04-10 13:18:42 +0300

    Fix tf first commit (#1)
    * added TF files
    
    * added GCP access
    
    * import mysql
    
    * mysql01
    
    * mysql02
    
    * mysql03
    
    * mysql04
    
    * mysql 05
    
    * mysql 06
    
    * mysql 07
    
    * asasas
    
    * asasas
    
    * asasas
    
    * asasas
    
    * asasasas
    
    * assasaasas
    
    * asaaasasass
    
    * aasasasass
    
    * asasas
    
    * asasasa
    
    * asasas
    
    * asasas
    
    * asaasasa
    
    * dgsdssfs
    
    * ljljljlkj
    
    * ggfhfgfghfh
    
    * fddgdgddgdgd
    
    * sfsafafaffa
    
    * wqeqeqeqe
    
    * adasdasdad
    
    * aaSssSsS
    
    * asasasas
    
    * sdsdsdsd
    
    * sssdsdsds
    
    * dgdgdfdgd
    
    * asasas
    
    * asasasasas
    
    * asasasasasasa
    
    * asasasassa
    
    * aaaasasada
    
    * adadasdasdas
    
    * fgdfgdgdg
    
    * dfsffsfs
    
    * ssaddadadaa
    
    * asadadadad
    
    * dsfsfssf
    
    * sjadgjagdjasdgj
    
    * adadasSs
    
    * asasas
    
    * asasasasasa
    
    * jhhjhjj
    
    * asasaas
    
    * asasasasas
    
    * asasasas
    
    * asasasas
    
    * asasasasa
    
    * asasas
    
    * addadada
    
    * saadasadads
    
    * asdadadada
    
    * asSsSs
    
    * asaadasdasdsa
    
    * Sssasa
    
    * asdsdadaasd
    
    * asasasasasas
    
    * asasaas
    
    * adsdsadada
    
    * asasasa
    
    * dgdfgdgdg
    
    * asasasa
    
    * asadadsa
    
    * asdasdsaasdasd
    
    * adasdasdadsa
    
    * kKkshASkskSH
    
    * asSsS
    
    * asadasdsda
    
    * assSsaSAS
    
    * AasSs
    
    * asasaaasd
    
    * asasasas
    
    * adsadsaasdsa
    
    * asasasas
    
    * Update main.tf

---
+++ infra/vpc/modules/cloudsql-postgres/outputs.tf
@@ -0,0 +1,18 @@
+output "db_dns_name" {
+  description = "The DNS name of the DB instance"
+  value       = google_sql_database_instance.main.dns_name
+}
+
+output "db_connection_name" {
+  description = "The connection name of the instance"
+  value       = google_sql_database_instance.main.connection_name
+}
+
+output "db_password_secret_name" {
+  value = google_secret_manager_secret.db_secret.name
+}
+
+output "service_attachment_url" {
+  description = "The URI that points to the service attachment of the instance"
+  value       = google_sql_database_instance.main.psc_service_attachment_link
+}
